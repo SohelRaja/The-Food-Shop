d2672a11a01df603351a8e943bd59d3afc21fb5a
SohelRaja <rohan.nsec@gmail.com>
2020-04-17 16:18:09 +0530

    adding the main component and reorganizing the all components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -34,17 +34,19 @@ class DishDetail extends Component{
     renderDish(dish) {
         if (dish != null)
             return(
-                <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={dish.image} alt={dish.name} />
-                            <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
+                <div className="container">
+                    <div className="row">
+                        <div  className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg top src={dish.image} alt={dish.name} />
+                                <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        {this.renderComments(dish.comments)}
                     </div>
-                    {this.renderComments(dish.comments)}
                 </div>
             );
         else
