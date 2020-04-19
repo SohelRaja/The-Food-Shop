0f11985791d1c8becc88fbd7eb30cf369853e05b
SohelRaja <rohan.nsec@gmail.com>
2020-04-19 10:06:54 +0530

    Navlink Link added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+
 import Menu from './MenuComponent';
 import './../App.css';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
@@ -18,16 +21,27 @@ class Main extends Component{
         this.setState({ selectedDish: dishID});
     }
     render(){
+        const HomePage = () => {
+            return(
+                <Home 
+                />
+            );
+        }
         return (
         <div>
             <Header />
-            <Menu 
+            {/* <Menu 
                 dishes={this.state.dishes} 
                 onClick={(dishID) => this.onDishSelect(dishID)}    
             />
             <DishDetail 
                 dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} 
-            />
+            /> */}
+            <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Redirect to="/home" />
+            </Switch>
             <Footer />
         </div>
         );
